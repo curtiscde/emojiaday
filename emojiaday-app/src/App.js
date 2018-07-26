f98503e2ab2a04aa4371abccfc8cbb4894c4ce7d
Curtis Timson <curt@live.co.uk>
2018-07-26 13:29:57 +0100

    Add AuthCallback component

--- emojiaday-app/src/App.js
+++ emojiaday-app/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Login from './components/Login';
+import AuthCallback from './components/AuthCallback';
 
 class App extends Component {
   render() {
@@ -10,7 +11,10 @@ class App extends Component {
       <div>
         <NavBar/>
         <BrowserRouter>
-          <Route exact path="/" render={() => (<Login/>)} />
+          <div>
+            <Route exact path="/" render={() => (<Login/>)} />
+            <Route exact path="/callback" render={() => (<AuthCallback/>)} />
+          </div>
         </BrowserRouter>
       </div>
     );
