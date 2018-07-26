0dabd3cb494ded9ac2a1c0d1c797671f1f77fa99
Curtis Timson <curt@live.co.uk>
2018-07-26 22:03:37 +0100

    Add AddEntry component

---
+++ emojiaday-app/src/components/AddEntry.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+export default class AddEntry extends Component {
+  render(){
+    return (
+      <Card>
+        <CardContent>
+          <Typography variant="title">Add Entry</Typography>
+          <Typography variant="subheading">Add your emoji of the day by selecting from the emoji picker below</Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Submit</Button>
+        </CardActions>
+      </Card>
+    )
+  }
+}
