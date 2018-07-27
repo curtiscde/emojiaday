67ac9842ece019f8426ee1b924591715fc6f9da8
Curtis Timson <curt@live.co.uk>
2018-07-27 14:56:30 +0100

    Prevent adding entry unless emoji data supplied

--- emojiaday-service/routes/entry-post.js
+++ emojiaday-service/routes/entry-post.js
@@ -5,41 +5,42 @@ import authHelper from '../helpers/auth-helper';
 
 module.exports = (apiRoutes) => {
 
-    // apiRoutes.get('/entry/day', (req, res) => {
-    //     console.log('GET entry');
-    //     res.json();
-    // });
-
     apiRoutes.post('/entry/day', authHelper.jwtCheck, (req, res) => {
-        console.log('📩 POST entry day', req.params.day);
+        console.log('📩 POST entry day');
 
         const userid = req.user.sub;
-        const date = moment(req.body.date).toDate();
+        const date = moment().toDate();
         const emoji = req.body.emoji;
+
+        if (!emoji){
+            res.status(500).send('Missing emoji data');
+        }
+        else{
+       
+            entryHelper.getEntryByDateUser(date, userid).then(data => {
+    
+                if (data.length){
+                    res.status(500).send(`Entry already exists for ${userid} on ${date}`);
+                }
+                else{
+                    Entry.create({
+                        userid: userid,
+                        emoji: emoji,
+                        date: date
+                    }, (err, emoji) => {
+                        if (err) {
+                            res.status(500).send(err);
+                        }
+                        else{
+                            res.json(emoji);
+                        }
+                    });
+                }
+    
+            });
+
+        }
         
-        entryHelper.getEntryByDateUser(date, userid).then(data => {
-
-            if (data.length){
-                res.status(500).send(`Entry already exists for ${userid} on ${date}`);
-            }
-            else{
-                Entry.create({
-                    userid: userid,
-                    emoji: emoji,
-                    date: date
-                }, (err, emoji) => {
-                    if (err) {
-                        res.status(500).send(err);
-                    }
-                    else{
-                        res.json(emoji);
-                    }
-                });
-            }
-
-        });
-
-
         
     });
 
