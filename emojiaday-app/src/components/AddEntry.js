8bf4d11d0b6c7a2f0b64dab27c24d9894a6fc0c2
Curtis Timson <curt@live.co.uk>
2018-07-27 10:55:49 +0100

    Add post to service call

--- emojiaday-app/src/components/AddEntry.js
+++ emojiaday-app/src/components/AddEntry.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -6,6 +7,7 @@ import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import 'emoji-mart/css/emoji-mart.css'
 import { Picker } from 'emoji-mart'
+import Config from '../config';
 
 export default class AddEntry extends Component {
 
@@ -30,6 +32,14 @@ export default class AddEntry extends Component {
     const emojiId = this.state.emoji;
 
     console.log('emojiid', emojiId);
+
+    axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('id_token')}`;
+    axios.post(`${Config.serviceUri}/api/entry/day`, {
+      day: '20180801',
+      emoji: emojiId
+    }).then(data => {
+      console.log(data);
+    });
     
   };
 
