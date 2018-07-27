7f71ab9974be7a040413f58f1fd38ec0ea2e165a
Curtis Timson <curt@live.co.uk>
2018-07-27 13:16:41 +0100

    Sticky footer

--- emojiaday-app/src/components/Footer.js
+++ emojiaday-app/src/components/Footer.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import './Footer.css';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 
 export default class Footer extends Component {
     render(){
         return (
             <footer>
-                Emojiaday
+                <BottomNavigation
+                    // onChange={this.handleChange}
+                    showLabels
+                    // className={classes.root}
+                >
+                    <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+                    <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+                    <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+                </BottomNavigation>
             </footer>
         )
     }
