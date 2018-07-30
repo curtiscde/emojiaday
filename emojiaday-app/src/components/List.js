72f0ad3be9bd743a764d4bc34532ff03f4ea925e
Curtis Timson <curt@live.co.uk>
2018-07-30 12:46:28 +0100

    Add getEntries call to List component

--- emojiaday-app/src/components/List.js
+++ emojiaday-app/src/components/List.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Config from '../config';
 
 export default class List extends Component {
+
+    getEntries(){
+        axios.get(`${Config.serviceUri}/api/entries/user`)
+            .then(res => {
+                console.log(res);
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
+    }
+
     render(){
+
+        this.getEntries();
+
         return (
             <div>
                 List of recent emojis...
