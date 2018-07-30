5649a7c82b7abfbb6e8cbcb2ff983c19e5c39266
Curtis Timson <curt@live.co.uk>
2018-07-30 20:10:08 +0100

    Add entries to calendar tileContent

--- emojiaday-app/src/components/CalendarView.js
+++ emojiaday-app/src/components/CalendarView.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Calendar from 'react-calendar';
 import Config from '../config';
+import { Emoji } from 'emoji-mart'
 
 export default class CalendarView extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      entries: null
+    };
+  }
 
     getEntries(){
         axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('access_token')}`;
         axios.get(`${Config.serviceUri}/api/entries/user`)
             .then(res => {
-                console.log(res);
+                this.setState({
+                  entries: res.data
+                });
             })
             .catch(function (error) {
               console.log(error);
@@ -21,7 +32,8 @@ export default class CalendarView extends Component {
         this.getEntries();
 
         const addTileContent = ({date, view}) => {
-          return view === 'month' && date.getDay() === 0 ? <p>It's Sunday!</p> : null
+          const entry = this.state.entries && this.state.entries.filter(entry => new Date(entry.date).getDate() === date.getDate());
+          return view === 'month' && entry && entry.length ? <Emoji emoji={entry[0].emoji} set='emojione' size={16} /> : null
         };
 
         return (
