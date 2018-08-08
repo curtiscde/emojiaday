ac232b2b29d2994a90e36aeda6eeddc8f0ca8353
Curtis Timson <curt@live.co.uk>
2018-08-08 19:54:31 +0100

    Add entry with index

--- emojiaday-service/routes/entry-post.js
+++ emojiaday-service/routes/entry-post.js
@@ -2,45 +2,99 @@ import moment from 'moment';
 import Entry from '../models/entry';
 import entryHelper from '../helpers/entry-helper';
 import authHelper from '../helpers/auth-helper';
+import { rejects } from 'assert';
 
 module.exports = (apiRoutes) => {
 
+    //Required Params
+    // userid (req.user.sub)
+    // index
+    // emoji
     apiRoutes.post('/entry/day', authHelper.jwtCheck, (req, res) => {
         console.log('📩 POST entry day');
 
         const userid = req.user.sub;
         const date = moment().toDate();
+        const index = req.body.index;
         const emoji = req.body.emoji;
+        
 
         if (!emoji){
             res.status(500).send('Missing emoji data');
+        }
+        else if (!date){
+            res.status(500).send('Missing date data');
+        }
+        else if (!index){
+            res.status(500).send('Missing index data');
         }
         else{
 
             const startOfDay = moment(date).startOf('day');
             const endOfDay = moment(date).endOf('day');
 
-            Entry.findOneAndUpdate({
-                userid: userid,
-                date: {
-                    $gte: startOfDay.toDate(),
-                    $lt: endOfDay.toDate()
-                }
-            }, {
-                userid: userid,
-                emoji: emoji,
-                date: date
-            }, {
-                new: true,
-                upsert: true
+            Entry.find({
+                $and: [
+                    {
+                        userid: userid,
+                        date: {
+                            $gte: startOfDay.toDate(),
+                            $lt: endOfDay.toDate()
+                        }
+                    },
+                    {
+                        $or: [
+                            { index: index },
+                            { emoji: emoji }
+                        ]
+                    }
+                ]
             }, (err, entry) => {
-                if (err) {
-                    res.status(500).send(err);
+                console.log('entry', entry);
+
+                if (err){
+                    res.status(500).send('Error');
+                }
+                else if(entry.length){
+                    res.status(500).send('Index or Emoji already exists');
                 }
                 else{
-                    res.json(entry);
+                    Entry.create({
+                        userid: userid,
+                        date: date,
+                        index: index,
+                        emoji: emoji,
+                    }, (e, newEntry) => {
+                        console.log('newEntry', newEntry);
+    
+                        res.json(newEntry);
+                    });
+
                 }
+
             });
+
+            // Entry.findOneAndUpdate({
+            //     userid: userid,
+            //     date: {
+            //         $gte: startOfDay.toDate(),
+            //         $lt: endOfDay.toDate()
+            //     }
+            // }, {
+            //     userid: userid,
+            //     emoji: emoji,
+            //     date: date
+            // }, {
+            //     new: true,
+            //     upsert: true
+            // }, (err, entry) => {
+            //     if (err) {
+            //         res.status(500).send(err);
+            //     }
+            //     else{
+            //         res.json(entry);
+            //     }
+            // });
 
         }
         
