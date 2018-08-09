03d2c348bf02eab044119de4c3d5fcc3753df54c
Curtis Timson <curt@live.co.uk>
2018-08-09 12:39:36 +0100

    Fix setting state on error

--- emojiaday-app/src/scenes/CalendarView.js
+++ emojiaday-app/src/scenes/CalendarView.js
@@ -26,19 +26,20 @@ export default class CalendarView extends Component {
   }
 
   getEntries(){
+    let self = this;
     axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('access_token')}`;
     axios.get(`${Config.serviceUri}/api/entries/user`)
         .then(res => {
-            this.setState({
-              ...this.state,
+          self.setState({
+              ...self.state,
               entries: res.data,
               entriesLoaded: true
             });
         })
-        .catch(function (error) {
+        .catch(error => {
           console.log(error);
-          this.setState({
-            ...this.state,
+          self.setState({
+            ...self.state,
             entriesLoaded: true
           });
         });
