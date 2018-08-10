198d831c8b5e8111cb12a44924e587a1bef830a1
Curtis Timson <curt@live.co.uk>
2018-08-10 11:41:25 +0100

    Emoji picker (#28)
    * Refactor emoji picker to new component
    
    * Hide emoji picker preview
    
    * Move EmojiPicker to new folder
    
    * Add 100% width to picker

---
+++ emojiaday-app/src/components/EmojiPicker/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Picker } from 'emoji-mart'
+import './styles.css';
+
+export default class EmojiPicker extends Component{
+
+  constructor(props){
+    super(props);
+
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+
+  handleSelect(data){
+    const emojiId = data.id;
+    this.props.onSelect(emojiId);
+  }
+
+  render(){
+    return (
+      <Picker
+        emoji='grinning'
+        set='twitter'
+        title='Pick your emoji…'
+        onSelect={this.handleSelect}
+        showPreview={false}
+      />
+    )
+  }
+}
