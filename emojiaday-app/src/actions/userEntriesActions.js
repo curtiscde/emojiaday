a7c7c9dc3ce19b81e14e6f7a4e8e7065dfdaff7b
Curtis Timson <curt@live.co.uk>
2018-08-13 13:09:16 +0100

    Correct error dispatch event type

--- emojiaday-app/src/actions/userEntriesActions.js
+++ emojiaday-app/src/actions/userEntriesActions.js
@@ -3,14 +3,13 @@ import Config from '../config';
 
 export function fetchUserEntries(){
     return function(dispatch){
-        console.log('fetchUserEntries action');
         axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('access_token')}`;
         axios.get(`${Config.serviceUri}/api/entries/user`)
         .then(res => {
             dispatch({ type: 'FETCH_USER_ENTRIES_FULFILLED', payload: res.data })
         })
         .catch(error => {
-            dispatch({ type: 'FETCH_USER_ENTRIES_FULFILLED', payload: error })
+            dispatch({ type: 'FETCH_USER_ENTRIES_REJECTED', payload: error })
         });
     }
 }
