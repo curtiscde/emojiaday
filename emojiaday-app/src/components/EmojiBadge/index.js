9b244b030dee277613f4bae165516d242461214e
Curtis Timson <curt@live.co.uk>
2018-08-15 11:13:04 +0100

    Refactor top emojis (#31)
    * Fix linting issues
    
    * Refactor to new EmojiBadge component
    
    * Remove redundant references from TopEmojis
    
    * Return nothing if no emoji passed
    
    * Fix linting issues

---
+++ emojiaday-app/src/components/EmojiBadge/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
+import { Emoji } from 'emoji-mart';
+
+const EmojiBadge = (props) => {
+  const styles = {
+    badge: {
+      top: 1,
+      right: -15,
+      border: '2px solid #ccc',
+    },
+  };
+
+  const getBackgroundColor = (index) => {
+    switch (index) {
+      case 0: {
+        return 'rgba(255, 215, 0, 0.3)';
+      }
+      case 1: {
+        return 'rgba(211, 211, 211, 0.3)';
+      }
+      case 2: {
+        return 'rgba(205, 127, 50, 0.3)';
+      }
+      default: {
+        return null;
+      }
+    }
+  };
+
+  if (!props.emoji) return null;
+
+  return (
+    <IconButton key={props.emoji._id} style={{ backgroundColor: getBackgroundColor(props.index) }}>
+      {
+        props.emoji.count > 10
+          ? (
+            <Badge badgeContent={props.emoji.count} color="primary" classes={{ badge: styles.badge }}>
+              <Emoji emoji={props.emoji._id} set="twitter" size={32} />
+            </Badge>
+          )
+          : (
+            <Emoji emoji={props.emoji._id} set="twitter" size={32} />
+          )
+      }
+    </IconButton>
+  );
+}
+
+export default EmojiBadge;
--- emojiaday-app/src/scenes/DayView/components/TopEmojis.js
+++ emojiaday-app/src/scenes/DayView/components/TopEmojis.js
@@ -3,55 +3,28 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
-import { Emoji } from 'emoji-mart';
 import moment from 'moment';
+import EmojiBadge from '../../../components/EmojiBadge';
 
-export default class TopEmojis extends Component{
-    render(){
-
-        const styles = {
-            badge: {
-              top: 1,
-              right: -15,
-              border: `2px solid #ccc`,
-            },
-          };
-
-        const getBackgroundColor = index => {
-            return index === 0  ? 'rgba(255, 215, 0, 0.3)'
-                : index === 1 ? 'rgba(211, 211, 211, 0.3)'
-                : index === 2 ? 'rgba(205, 127, 50, 0.3)'
-                : '';
-        }
-
-        return (
-            <Card>
-                <CardContent>
-                    <Grid container spacing={24}>
-                        <Grid item xs={12}>
-                            <Typography variant="subheading" color="inherit">
-                                Top emojis for {moment(this.props.day).format('D MMMM YYYY')}
-                            </Typography>
-                        </Grid>
-                        {this.props.data.map((topEmoji, index) => (
-                            <Grid item xs={4} style={{ textAlign: 'center', paddingTop:12 }} key={index}>
-                                <IconButton key={topEmoji._id} style={{ backgroundColor: getBackgroundColor(index) }}>
-                                    {
-                                        topEmoji.count > 10 ?
-                                        <Badge badgeContent={topEmoji.count} color="primary" classes={{ badge: styles.badge }}>
-                                            <Emoji emoji={topEmoji._id} set='twitter' size={32} />
-                                        </Badge>
-                                        :
-                                        <Emoji emoji={topEmoji._id} set='twitter' size={32} />
-                                    }
-                                </IconButton>
-                            </Grid>
-                        ))}
-                    </Grid>
-                </CardContent>
-            </Card>
-        );
-    }
+export default class TopEmojis extends Component {
+  render() {
+    return (
+      <Card>
+        <CardContent>
+          <Grid container spacing={24}>
+            <Grid item xs={12}>
+              <Typography variant="subheading" color="inherit">
+                Top emojis for {moment(this.props.day).format('D MMMM YYYY')}
+              </Typography>
+            </Grid>
+            {this.props.data.map((topEmoji, index) => (
+              <Grid item xs={4} style={{ textAlign: 'center', paddingTop: 12 }} key={index}>
+                <EmojiBadge emoji={topEmoji} index={index} />
+              </Grid>
+            ))}
+          </Grid>
+        </CardContent>
+      </Card>
+    );
+  }
 }
