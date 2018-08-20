7c176241721a9483cb42f252c5276c678f3980da
Curtis Timson <curt@live.co.uk>
2018-08-20 06:35:46 +0100

    Add intial postUserEntry method to user entries actions

--- emojiaday-app/src/actions/userEntriesActions.js
+++ emojiaday-app/src/actions/userEntriesActions.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import Config from '../config';
+import ReactGA from 'react-ga';
 
 export function fetchUserEntries() {
   return function(dispatch){
@@ -12,4 +13,39 @@ export function fetchUserEntries() {
         dispatch({ type: 'FETCH_USER_ENTRIES_REJECTED', payload: error })
     });
   }
+}
+
+export function postUserEntry(emojiId, index, date) {
+  return (dispatch) => {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('access_token')}`;
+    axios.post(`${Config.serviceUri}/api/entry/day`, {
+      emoji: emojiId,
+      index: this.props.index,
+      date: moment().format('YYYYMMDD'),
+    }).then((res) => {
+      dispatch({ type: 'POST USER_ENTRY_FULFILLED', payload: res.data })
+      // this.setState({
+      //   ...this.state,
+      //   emoji: res.data.emoji,
+      //   entryId: res.data._id,
+      //   iconRequest: false
+      // });
+      ReactGA.event({
+        category: 'Emoji Entry',
+        action: 'Add',
+        label: emojiId
+      });
+      this.props.dispatch(userEntries.fetchUserEntries());
+    }).catch(error => {
+      dispatch({ type: 'POST USER_ENTRY_REJECTED', payload: res.data })
+      // this.setState({
+      //   ...this.state,
+      //   iconRequest: false
+      // });
+      ReactGA.event({
+        category: 'Error',
+        action: 'Emoji Entry Add'
+      });
+    });
+  };
 }
