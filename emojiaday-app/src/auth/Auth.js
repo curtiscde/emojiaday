9adebe5ccf1552e6c365b8b272594cdc1f9e40a8
Curtis Timson <curt@live.co.uk>
2018-08-21 21:15:16 +0100

    Renew session when expired

--- emojiaday-app/src/auth/Auth.js
+++ emojiaday-app/src/auth/Auth.js
@@ -4,6 +4,7 @@ import config from '../config';
 import ReactGA from 'react-ga';
 
 export default class Auth {
+  tokenRenewalTimeout;
 
   auth0 = new auth0.WebAuth({
     domain: config.auth0.domain,
@@ -14,6 +15,11 @@ export default class Auth {
     scope: 'openid'
   });
 
+  constructor() {
+    console.log('const');
+    this.scheduleRenewal();
+  }
+
   login() {
     this.auth0.authorize();
   }
@@ -22,6 +28,7 @@ export default class Auth {
     localStorage.removeItem('access_token');
     localStorage.removeItem('id_token');
     localStorage.removeItem('expires_at');
+    clearTimeout(this.tokenRenewalTimeout);
     ReactGA.event({
       category: 'Authentication',
       action: 'Logout'
@@ -42,18 +49,48 @@ export default class Auth {
   }
 
   setSession(authResult) {
-    // Set the time that the Access Token will expire at
     let expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
     localStorage.setItem('access_token', authResult.accessToken);
     localStorage.setItem('id_token', authResult.idToken);
     localStorage.setItem('expires_at', expiresAt);
+
+    this.scheduleRenewal();
   }
 
   isAuthenticated() {
     // Check whether the current time is past the 
     // Access Token's expiry time
     let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    console.log(expiresAt - new Date().getTime());
     return new Date().getTime() < expiresAt;
   }
 
+  renewToken() {
+    this.auth0.checkSession({},
+      (err, result) => {
+        if (err) {
+          console.log(
+            `Could not get a new token (${err.error}: ${err.error_description}).`
+          );
+          history.replace('/');
+        } else {
+          this.setSession(result);
+          console.log(`Successfully renewed auth!`);
+        }
+      }
+    );
+  }
+
+  scheduleRenewal() {
+    console.log('scheduleRenewal fired');
+    const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    const delay = expiresAt - Date.now();
+    console.log('schedrenew-delay', delay);
+    if (delay > 0) {
+      this.tokenRenewalTimeout = setTimeout(() => {
+        this.renewToken();
+        console.log(`renew scheduled in ${delay}`);
+      }, delay);
+    }
+  }
 };
