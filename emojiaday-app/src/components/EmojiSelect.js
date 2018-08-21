6b9f0b21df3abd32152b1627c0ea9be2f67de392
Curtis Timson <curt@live.co.uk>
2018-08-21 10:01:12 +0100

    Fix linting issues

--- emojiaday-app/src/components/EmojiSelect.js
+++ emojiaday-app/src/components/EmojiSelect.js
@@ -3,21 +3,20 @@ import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
 import AddCircleOutlined from '@material-ui/icons/AddCircleOutlined';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import { Emoji } from 'emoji-mart'
+import { Emoji } from 'emoji-mart';
 import moment from 'moment';
-import { connect } from "react-redux"
-import * as userEntries from '../actions/userEntriesActions';
+import { connect } from 'react-redux';
 
-class EmojiSelect extends Component{
-  constructor(props){
+class EmojiSelect extends Component {
+  constructor(props) {
     super(props);
 
     this.isToday = (this.props.day === moment().format('YYYYMMDD'));
-  
+
     this.handleIconClick = this.handleIconClick.bind(this);
   }
 
-  handleIconClick(){
+  handleIconClick() {
     this.props.onClick(this.props.day, this.props.index, this.props.entry);
   }
 
