357becea1a2cd5b02a5ff5f7854c4c7f81b8bc6d
Curtis Timson <curt@live.co.uk>
2018-08-23 22:19:12 +0100

    Remove redundant auth.js

--- emojiaday-app/src/auth/Auth.js
+++
@@ -1,8 +0,0 @@
-export default class Auth {
-  isAuthenticated() {
-    // Check whether the current time is past the 
-    // Access Token's expiry time
-    let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
-    return new Date().getTime() < expiresAt;
-  }
-};
